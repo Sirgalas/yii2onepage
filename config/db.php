7cdf0c7255445b1d95136481309bd6c2ee076500
sirgalas <hudos@rambler.ru>
2018-03-09 12:56:27 +0300

    не получилось с пользователем

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=admin_onepage',
-    'username' => 'admin_root',
-    'password' => '3sFG8c',
+    'dsn' => 'mysql:host=localhost;dbname=onePage',
+    'username' => 'root',
+    'password' => '1',
     'charset' => 'utf8',
 ];
