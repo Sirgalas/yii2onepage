2a03101e308127f7c85ae8fbec69e6ac703b39e3
sirgalas <hudos@rambler.ru>
2018-03-09 12:46:54 +0300

    test

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=onePage',
-    'username' => 'root',
-    'password' => '1',
+    'dsn' => 'mysql:host=localhost;dbname=admin_onepage',
+    'username' => 'admin_root',
+    'password' => '3sFG8c',
     'charset' => 'utf8',
 ];
